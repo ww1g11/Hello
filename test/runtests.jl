3b475faa1a092d130b2a209a925d9547bf0d6174
Weiwei Wang <wangweiwei@ahu.edu.cn>
2019-05-18 12:34:29 +0800

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,6 @@
 using Hello
 using Test
-using CuArrays
 
-a = cuzeros(10)
-b = cuzeros(10)
-
-add2(a)
-
-mesh =  Hello.FDMeshGPU(dx=1.1e-9, nx=10, ny=2)
-@test isapprox(mesh.dx*1.0, 1.1e-9, rtol=1e-7) #FIXME: shall we always use Float64 for dx, dy and dz?
-@test mesh.nx == 10
-#println(mesh.ngbs)
-@test isapprox(mesh.volume*1.0, 1.1e-27, rtol=1e-7)
-
-Hello.add_gpu(a,b)
 Hello.greet()
 
 Hello.test_gpu()
