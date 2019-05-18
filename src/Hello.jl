667eb8c5409051df518188ed0b1d5c6754a022ac
Weiwei Wang <wangweiwei@ahu.edu.cn>
2019-05-18 12:29:32 +0800

    update

--- src/Hello.jl
+++ src/Hello.jl
@@ -2,35 +2,6 @@ module Hello
 
 abstract type MeshGPU end
 
-using CuArrays
-using CUDAnative
-
-const _cuda_using_double = Ref(false)
-
-struct FDMeshGPU{T <: AbstractFloat} <: MeshGPU
-  dx::T
-  dy::T
-  dz::T
-  nx::Int64
-  ny::Int64
-  nz::Int64
-  nxyz::Int64
-  xperiodic::Bool
-  yperiodic::Bool
-  zperiodic::Bool
-  volume::T
-end
-
-function FDMeshGPU(;dx=1e-9, dy=1e-9, dz=1e-9, nx=10, ny=10, nz=1, pbc="open")
-  nxyz = nx*ny*nz
-  volume = dx*dy*dz
-  Float = _cuda_using_double.x ? Float64 : Float32
-  xperiodic = 'x' in pbc ? true : false
-  yperiodic = 'y' in pbc ? true : false
-  zperiodic = 'z' in pbc ? true : false
-  return FDMeshGPU(Float(dx), Float(dy), Float(dz), nx, ny, nz, nxyz, xperiodic, yperiodic, zperiodic, Float(volume))
-end
-
 add2(a) = a.+2
 
 add_gpu(a,b) = a.+b
